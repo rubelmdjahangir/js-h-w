a3ac3111c1725ae2f6baadd73087267e14976def
rubelmdjahangir <114277370+rubelmdjahangir@users.noreply.github.com>
2022-12-08 14:22:06 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function passToggle () {
+
+if (document.getElementById('password').type ==='password')
+{document.getElementById('password').type ='text'}
+else {
+    document.getElementById('password').type = 'password'
+}
